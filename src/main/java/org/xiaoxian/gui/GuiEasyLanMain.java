bedd36bbc978f937dec16e5f6fb22276dd386fc2
ItIsMF <zmj070102@outlook.com>
2025-12-06 04:59:48 +0800

    Revert "Test"
    This reverts commit ed1d5c9a5e7fd23b3734cdadb2c6889d4252718a.

--- src/main/java/org/xiaoxian/gui/GuiEasyLanMain.java
+++ src/main/java/org/xiaoxian/gui/GuiEasyLanMain.java
@@ -156,10 +156,6 @@ public class GuiEasyLanMain extends GuiScreen {
                 }
             } else if (button instanceof CheckBoxButtonUtil) {
                 CheckBoxButtonUtil checkBox = (CheckBoxButtonUtil) button;
-
-                // 🔧 关键：切换状态并更新变量
-                checkBox.toggle(); // 这现在可以正常工作了！
-
                 switch (button.id) {
                     case 10: allowPVP = checkBox.isChecked(); break;
                     case 11: onlineMode = checkBox.isChecked(); break;
@@ -173,12 +169,9 @@ public class GuiEasyLanMain extends GuiScreen {
                     case 30: HttpAPI = checkBox.isChecked(); break;
                     case 31: LanOutput = checkBox.isChecked(); break;
                 }
-
-                System.out.println("[EasyLAN] Checkbox " + button.id + " toggled to: " + checkBox.isChecked());
             }
         } catch (Exception e) {
             System.err.println("[EasyLAN] Error in actionPerformed: " + e.getMessage());
-            e.printStackTrace();
         }
     }
 
@@ -227,13 +220,11 @@ public class GuiEasyLanMain extends GuiScreen {
                         case 23: checkBox.setChecked(SaveCommand); break;
                         case 30: checkBox.setChecked(HttpAPI); break;
                         case 31: checkBox.setChecked(LanOutput); break;
-                        default: break;
                     }
                 }
             }
         } catch (Exception e) {
             System.err.println("[EasyLAN] Error updating GUI config: " + e.getMessage());
-            e.printStackTrace();
         }
     }
 
--- src/main/java/org/xiaoxian/util/CheckBoxButtonUtil.java
+++ src/main/java/org/xiaoxian/util/CheckBoxButtonUtil.java
@@ -1,99 +1,36 @@
 package org.xiaoxian.util;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
-import org.lwjgl.opengl.GL11;
 
-public class CheckBoxButtonUtil extends GuiButton {
-    private boolean checked;
-    private boolean hovered;
+import javax.annotation.Nonnull;
+import java.awt.*;
 
-    public CheckBoxButtonUtil(int id, int x, int y, boolean initialState, int width, int height) {
-        super(id, x, y, width, height, "");
-        this.checked = initialState;
+public class CheckBoxButtonUtil extends ButtonUtil {
+    private boolean isChecked;
+
+    public CheckBoxButtonUtil(int buttonId, int x, int y, boolean isChecked, int width, int height) {
+        super(buttonId, x, y, width, height, "");
+        this.isChecked = isChecked;
     }
 
-    // 🔧 获取选中状态
+    @Override
+    public void drawButton(@Nonnull Minecraft mc, int mouseX, int mouseY) {
+        super.drawButton(mc, mouseX, mouseY);
+
+        Color color = this.isChecked ? Color.WHITE: Color.GRAY;
+        DrawUtil.drawRect(this.xPosition + 2, this.yPosition + 2, this.width - 4, this.height - 4, color);
+    }
+
     public boolean isChecked() {
-        return checked;
+        return isChecked;
     }
 
-    // 🔧 设置选中状态
-    public void setChecked(boolean checked) {
-        this.checked = checked;
+    public void toggleChecked() {
+        this.isChecked = !this.isChecked;
     }
 
-    // 🔧 关键方法：切换选中状态
-    public void toggle() {
-        this.checked = !this.checked;
+    public void setChecked(boolean isChecked) {
+        this.isChecked = isChecked;
     }
 
-    // 🔧 绘制复选框
-    @Override
-    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
-        if (this.visible) {
-            // 检查鼠标是否悬停
-            this.field_146123_n = mouseX >= this.xPosition && mouseY >= this.yPosition
-                && mouseX < this.xPosition + this.width
-                && mouseY < this.yPosition + this.height;
-
-            this.hovered = this.field_146123_n;
-
-            // 绘制复选框背景
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-
-            // 绘制外框
-            drawRect(this.xPosition, this.yPosition,
-                this.xPosition + this.width, this.yPosition + this.height,
-                this.hovered ? 0xFFAAAAAA : 0xFF888888);
-
-            // 绘制内框（背景）
-            drawRect(this.xPosition + 1, this.yPosition + 1,
-                this.xPosition + this.width - 1, this.yPosition + this.height - 1,
-                0xFF000000);
-
-            // 如果选中，绘制勾选标记
-            if (this.checked) {
-                drawCheckMark();
-            }
-
-            this.mouseDragged(mc, mouseX, mouseY);
-        }
-    }
-
-    // 🔧 可选：绘制勾号
-    private void drawCheckMark() {
-        // 绘制勾号的线条
-        GL11.glPushMatrix();
-        GL11.glTranslatef(this.xPosition + 2, this.yPosition + 2, 0);
-        GL11.glLineWidth(2.0f);
-        GL11.glBegin(GL11.GL_LINES);
-        GL11.glVertex2f(2, 6);
-        GL11.glVertex2f(6, 10);
-        GL11.glVertex2f(6, 10);
-        GL11.glVertex2f(12, 2);
-        GL11.glEnd();
-        GL11.glPopMatrix();
-    }
-
-    // 🔧 处理鼠标点击
-    @Override
-    public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
-        if (this.enabled && this.visible) {
-            if (mouseX >= this.xPosition && mouseY >= this.yPosition &&
-                mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height) {
-
-                // 点击时切换状态
-                this.toggle();
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // 🔧 处理鼠标释放（可选）
-    @Override
-    public void mouseReleased(int mouseX, int mouseY) {
-        // 可以在这里添加释放时的逻辑
-    }
 }
