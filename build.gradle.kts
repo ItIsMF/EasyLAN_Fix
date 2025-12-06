db6e45face7b388ac9205f72fb0990c619108a6d
ItIsMF <zmj070102@outlook.com>
2025-12-07 06:10:18 +0800

    info update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,14 @@
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
+
+group = "com.xiaoxian.easylan"
+version = "1.5fix"
+val archivesBaseName = "EasyLAN_Fix"
+
+tasks.jar {
+    archiveBaseName.set("EasyLAN_Fix")           // 基础名称
+    archiveVersion.set("1.5fix")            // 版本号
+    archiveClassifier.set("")       // 分类器（可选）
+    archiveExtension.set("jar")            // 扩展名（默认）
+}
