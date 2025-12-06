56a6e07093ee8146ac6d01ebe37b23e65d3306d6
ItIsMF <zmj070102@outlook.com>
2025-12-07 02:38:36 +0800

    GUI Fix

--- src/main/java/org/xiaoxian/gui/GuiEasyLanMain.java
+++ src/main/java/org/xiaoxian/gui/GuiEasyLanMain.java
@@ -4,11 +4,13 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
 import org.xiaoxian.util.ButtonUtil;
 import org.xiaoxian.util.CheckBoxButtonUtil;
 import org.xiaoxian.util.ConfigUtil;
 import org.xiaoxian.util.TextBoxUtil;
-import java.awt.*;
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.PositionedSoundRecord;
 import java.util.ArrayList;
 import java.util.List;
 import static org.xiaoxian.EasyLAN.*;
@@ -31,67 +33,41 @@ public class GuiEasyLanMain extends GuiScreen {
         buttonList.add(new ButtonUtil(0, this.width / 2 + 70, this.height - 25, 100, 20, I18n.format("easylan.back")));
         buttonList.add(new ButtonUtil(1, this.width / 2 - 50, this.height - 25, 100, 20, I18n.format("easylan.save")));
 
-        // 游戏规则设置
-        buttonList.add(new CheckBoxButtonUtil(10, this.width / 2 - 145, 60, allowPVP, 20, 20));
-        buttonList.add(new CheckBoxButtonUtil(11, this.width / 2 - 145, 85, onlineMode, 20, 20));
-        buttonList.add(new CheckBoxButtonUtil(12, this.width / 2 - 145, 110, spawnAnimals, 20, 20));
-        buttonList.add(new CheckBoxButtonUtil(13, this.width / 2 - 145, 135, spawnNPCs, 20, 20));
-        buttonList.add(new CheckBoxButtonUtil(14, this.width / 2 - 145, 160, allowFlight, 20, 20));
+        // 游戏规则设置 - 使用绝对坐标，避免计算错误
+        int baseX = this.width / 2 - 145;
+        int baseY = 60;
+        int spacing = 25;
+
+        buttonList.add(new CheckBoxButtonUtil(10, baseX, baseY, allowPVP, 20, 20));
+        buttonList.add(new CheckBoxButtonUtil(11, baseX, baseY + spacing, onlineMode, 20, 20));
+        buttonList.add(new CheckBoxButtonUtil(12, baseX, baseY + spacing * 2, spawnAnimals, 20, 20));
+        buttonList.add(new CheckBoxButtonUtil(13, baseX, baseY + spacing * 3, spawnNPCs, 20, 20));
+        buttonList.add(new CheckBoxButtonUtil(14, baseX, baseY + spacing * 4, allowFlight, 20, 20));
 
         // 指令支持
-        buttonList.add(new CheckBoxButtonUtil(20, this.width / 2 - 25, 60, whiteList, 20, 20));
-        buttonList.add(new CheckBoxButtonUtil(21, this.width / 2 - 25, 85, BanCommand, 20, 20));
-        buttonList.add(new CheckBoxButtonUtil(22, this.width / 2 - 25, 110, OpCommand, 20, 20));
-        buttonList.add(new CheckBoxButtonUtil(23, this.width / 2 - 25, 135, SaveCommand, 20, 20));
+        baseX = this.width / 2 - 25;
+        baseY = 60;
+        buttonList.add(new CheckBoxButtonUtil(20, baseX, baseY, whiteList, 20, 20));
+        buttonList.add(new CheckBoxButtonUtil(21, baseX, baseY + spacing, BanCommand, 20, 20));
+        buttonList.add(new CheckBoxButtonUtil(22, baseX, baseY + spacing * 2, OpCommand, 20, 20));
+        buttonList.add(new CheckBoxButtonUtil(23, baseX, baseY + spacing * 3, SaveCommand, 20, 20));
 
         // 其他设置
-        buttonList.add(new CheckBoxButtonUtil(30, this.width / 2 + 95, 60, HttpAPI, 20, 20));
-        buttonList.add(new CheckBoxButtonUtil(31, this.width / 2 + 95, 85, LanOutput, 20, 20));
+        baseX = this.width / 2 + 95;
+        baseY = 60;
+        buttonList.add(new CheckBoxButtonUtil(30, baseX, baseY, HttpAPI, 20, 20));
+        buttonList.add(new CheckBoxButtonUtil(31, baseX, baseY + spacing, LanOutput, 20, 20));
 
-        // MOTD 文本框 - 添加安全初始化
-        try {
-            MotdTextBox = new TextBoxUtil(100, mc.fontRenderer, this.width / 2 - 70, 185, 230, 20);
-            if (MotdTextBox != null) {
-                MotdTextBox.setMaxStringLength(100);
-                safeSetMotdText();  // 使用安全方法设置文本
-            }
-        } catch (Exception e) {
-            System.err.println("[EasyLAN] Error creating MOTD text box: " + e.getMessage());
-        }
+        // MOTD
+        MotdTextBox = new TextBoxUtil(100, mc.fontRenderer, this.width / 2 - 70, 185, 230, 20);
+        MotdTextBox.setMaxStringLength(100);
 
         updateGuiConfig();
     }
 
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+    private void drawLabelStrings() {
         try {
-            this.drawDefaultBackground();
-
-            // 安全绘制 MOTD 文本框
-            if (MotdTextBox != null) {
-                try {
-                    MotdTextBox.drawTextBox();
-                } catch (Exception e) {
-                    System.err.println("[EasyLAN] Error drawing MOTD text box: " + e.getMessage());
-                }
-            }
-
-            // 绘制标签文本 - 添加 null 保护
-            safeDrawStrings();
-
-            super.drawScreen(mouseX, mouseY, partialTicks);
-
-        } catch (Exception e) {
-            System.err.println("[EasyLAN] Critical error in drawScreen: " + e.getMessage());
-            // 降级处理：至少绘制基本界面
-            drawCenteredString(fontRendererObj, "GUI Error - Please restart", width/2, height/2, 0xFF0000);
-        }
-    }
-
-    // 安全绘制字符串的方法
-    private void safeDrawStrings() {
-        try {
-            drawString(mc.fontRenderer, safeGetString("easylan.title"), this.width / 2, 10, 0xFFFFFF);
+            if (mc.fontRenderer == null) return;
 
             // 游戏规则
             drawString(mc.fontRenderer, safeGetString("easylan.text.setting1"), this.width / 2 - 165, 35, 0x33CCFF);
@@ -117,8 +93,39 @@ public class GuiEasyLanMain extends GuiScreen {
             drawString(mc.fontRenderer, safeGetString("easylan.text.motd"), this.width / 2 - 70, 170, 0xFFFFFF);
 
         } catch (Exception e) {
-            System.err.println("[EasyLAN] Error drawing strings: " + e.getMessage());
+            System.err.println("[EasyLAN] Error drawing label strings: " + e.getMessage());
         }
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        // 首先绘制背景
+        this.drawDefaultBackground();
+
+        try {
+            // 绘制所有按钮（包括复选框）
+            if (buttonList != null) {
+                for (GuiButton button : buttonList) {
+                    if (button != null) {
+                        button.drawButton(mc, mouseX, mouseY);
+                    }
+                }
+            }
+
+            // 绘制文本框
+            if (MotdTextBox != null) {
+                MotdTextBox.drawTextBox();
+            }
+
+            // 绘制标签文本
+            drawLabelStrings();
+
+        } catch (Exception e) {
+            System.err.println("[EasyLAN] Error in drawScreen: " + e.getMessage());
+        }
+
+        // 最后调用父类方法
+        super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     // 安全获取字符串的方法
@@ -156,24 +163,32 @@ public class GuiEasyLanMain extends GuiScreen {
                 }
             } else if (button instanceof CheckBoxButtonUtil) {
                 CheckBoxButtonUtil checkBox = (CheckBoxButtonUtil) button;
+
+                // 只需要更新变量和配置，toggle() 已在 mousePressed 中调用
+                boolean newState = checkBox.isChecked();
+
                 switch (button.id) {
-                    case 10: allowPVP = checkBox.isChecked(); break;
-                    case 11: onlineMode = checkBox.isChecked(); break;
-                    case 12: spawnAnimals = checkBox.isChecked(); break;
-                    case 13: spawnNPCs = checkBox.isChecked(); break;
-                    case 14: allowFlight = checkBox.isChecked(); break;
-                    case 20: whiteList = checkBox.isChecked(); break;
-                    case 21: BanCommand = checkBox.isChecked(); break;
-                    case 22: OpCommand = checkBox.isChecked(); break;
-                    case 23: SaveCommand = checkBox.isChecked(); break;
-                    case 30: HttpAPI = checkBox.isChecked(); break;
-                    case 31: LanOutput = checkBox.isChecked(); break;
+                    case 10: allowPVP = newState; break;
+                    case 11: onlineMode = newState; break;
+                    case 12: spawnAnimals = newState; break;
+                    case 13: spawnNPCs = newState; break;
+                    case 14: allowFlight = newState; break;
+                    case 20: whiteList = newState; break;
+                    case 21: BanCommand = newState; break;
+                    case 22: OpCommand = newState; break;
+                    case 23: SaveCommand = newState; break;
+                    case 30: HttpAPI = newState; break;
+                    case 31: LanOutput = newState; break;
                 }
+
+                // 立即保存配置
+                saveConfig();
             }
         } catch (Exception e) {
             System.err.println("[EasyLAN] Error in actionPerformed: " + e.getMessage());
         }
     }
+
 
     @Override
     protected void keyTyped(char typedChar, int keyCode) {
@@ -189,15 +204,22 @@ public class GuiEasyLanMain extends GuiScreen {
     }
 
     @Override
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) {
-        try {
-            if (MotdTextBox != null) {
-                MotdTextBox.mouseClicked(mouseX, mouseY, mouseButton);
-                MotdText = MotdTextBox.getText();
+    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+
+        for (GuiButton button : buttonList) {
+            if (button.mousePressed(mc, mouseX, mouseY)) {
+                ISound sound = new PositionedSoundRecord(
+                    new ResourceLocation("random", "click"),  // 声音资源
+                    1.0F,  // 音量
+                    1.0F,  // 音调
+                    0.0F,  // x坐标
+                    0.0F,  // y坐标
+                    0.0F   // z坐标
+                );
+                actionPerformed(button);
+                return;
             }
-            super.mouseClicked(mouseX, mouseY, mouseButton);
-        } catch (Exception e) {
-            System.err.println("[EasyLAN] Error in mouseClicked: " + e.getMessage());
         }
     }
 
--- src/main/java/org/xiaoxian/util/CheckBoxButtonUtil.java
+++ src/main/java/org/xiaoxian/util/CheckBoxButtonUtil.java
@@ -1,36 +1,105 @@
 package org.xiaoxian.util;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import org.lwjgl.opengl.GL11;
 
-import javax.annotation.Nonnull;
-import java.awt.*;
+public class CheckBoxButtonUtil extends GuiButton {
+    private boolean checked;
+    private boolean hovered;
 
-public class CheckBoxButtonUtil extends ButtonUtil {
-    private boolean isChecked;
-
-    public CheckBoxButtonUtil(int buttonId, int x, int y, boolean isChecked, int width, int height) {
-        super(buttonId, x, y, width, height, "");
-        this.isChecked = isChecked;
-    }
-
-    @Override
-    public void drawButton(@Nonnull Minecraft mc, int mouseX, int mouseY) {
-        super.drawButton(mc, mouseX, mouseY);
-
-        Color color = this.isChecked ? Color.WHITE: Color.GRAY;
-        DrawUtil.drawRect(this.xPosition + 2, this.yPosition + 2, this.width - 4, this.height - 4, color);
+    public CheckBoxButtonUtil(int id, int x, int y, boolean initialState, int width, int height) {
+        super(id, x, y, width, height, "");
+        this.checked = initialState;
     }
 
     public boolean isChecked() {
-        return isChecked;
+        return checked;
     }
 
-    public void toggleChecked() {
-        this.isChecked = !this.isChecked;
+    public void setChecked(boolean checked) {
+        this.checked = checked;
     }
 
-    public void setChecked(boolean isChecked) {
-        this.isChecked = isChecked;
+    public void toggle() {
+        this.checked = !this.checked;
     }
 
+    @Override
+    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        if (this.visible) {
+            // 检查鼠标悬停状态
+            this.field_146123_n = (mouseX >= this.xPosition && mouseY >= this.yPosition &&
+                mouseX < this.xPosition + this.width &&
+                mouseY < this.yPosition + this.height);
+            this.hovered = this.field_146123_n;
+
+            // 保存 OpenGL 状态
+            GL11.glPushMatrix();
+            GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+
+            try {
+                // 确保正确的渲染状态
+                GL11.glEnable(GL11.GL_BLEND);
+                GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+                GL11.glDisable(GL11.GL_TEXTURE_2D);
+
+                // 绘制边框
+                int borderColor = this.hovered ? 0xFFA0A0A0 : 0xFF808080;
+                drawRect(this.xPosition, this.yPosition,
+                    this.xPosition + this.width, this.yPosition + 1,
+                    borderColor);
+                drawRect(this.xPosition, this.yPosition,
+                    this.xPosition + 1, this.yPosition + this.height,
+                    borderColor);
+                drawRect(this.xPosition + this.width - 1, this.yPosition,
+                    this.xPosition + this.width, this.yPosition + this.height,
+                    borderColor);
+                drawRect(this.xPosition, this.yPosition + this.height - 1,
+                    this.xPosition + this.width, this.yPosition + this.height,
+                    borderColor);
+
+                // 绘制内部背景
+                int backgroundColor = this.hovered ? 0xFFE0E0E0 : 0xFFFFFFFF;
+                drawRect(this.xPosition + 1, this.yPosition + 1,
+                    this.xPosition + this.width - 1, this.yPosition + this.height - 1,
+                    backgroundColor);
+
+                // 如果选中，绘制勾选标记
+                if (this.checked) {
+                    // 绘制绿色勾号
+                    drawRect(this.xPosition + 4, this.yPosition + this.height / 2 - 1,
+                        this.xPosition + this.width / 2 - 1, this.yPosition + this.height / 2 + 1,
+                        0xFF008000); // 水平线
+                    drawRect(this.xPosition + this.width / 2 - 1, this.yPosition + this.height / 2 - 1,
+                        this.xPosition + this.width - 4, this.yPosition + this.height / 2 + 1,
+                        0xFF008000); // 斜线
+                    drawRect(this.xPosition + 4, this.yPosition + this.height / 2 - 1,
+                        this.xPosition + this.width / 2 - 1, this.yPosition + this.height - 4,
+                        0xFF008000); // 垂直线
+                }
+
+            } finally {
+                // 恢复 OpenGL 状态
+                GL11.glEnable(GL11.GL_TEXTURE_2D);
+                GL11.glPopAttrib();
+                GL11.glPopMatrix();
+            }
+
+            this.mouseDragged(mc, mouseX, mouseY);
+        }
+    }
+
+    @Override
+    public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
+        if (this.enabled && this.visible) {
+            if (mouseX >= this.xPosition && mouseY >= this.yPosition &&
+                mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height) {
+
+                this.toggle();
+                return true;
+            }
+        }
+        return false;
+    }
 }
