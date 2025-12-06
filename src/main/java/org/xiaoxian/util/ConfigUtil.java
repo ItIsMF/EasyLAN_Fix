1e753da35ce6bbc7012c3c329d2377c3863bc256
ItIsMF <zmj070102@outlook.com>
2025-12-07 03:58:42 +0800

    config fix

--- src/main/java/org/xiaoxian/util/ConfigUtil.java
+++ src/main/java/org/xiaoxian/util/ConfigUtil.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
-
 import static org.xiaoxian.EasyLAN.*;
 
 public class ConfigUtil {
@@ -13,37 +12,45 @@ public class ConfigUtil {
 
     public static void load() {
         File file = new File(CONFIG_FILE);
+
         if (file.exists()) {
             try (InputStream input = Files.newInputStream(file.toPath())) {
                 properties.load(input);
-                allowPVP = Boolean.parseBoolean(ConfigUtil.get("pvp"));
-                onlineMode = Boolean.parseBoolean(ConfigUtil.get("online-mode"));
-                spawnAnimals = Boolean.parseBoolean(ConfigUtil.get("spawn-Animals"));
-                spawnNPCs = Boolean.parseBoolean(ConfigUtil.get("spawn-NPCs"));
-                allowFlight = Boolean.parseBoolean(ConfigUtil.get("allow-Flight"));
-                whiteList = Boolean.parseBoolean(ConfigUtil.get("whiteList"));
-                BanCommand = Boolean.parseBoolean(ConfigUtil.get("BanCommand"));
-                OpCommand = Boolean.parseBoolean(ConfigUtil.get("OpCommand"));
-                SaveCommand = Boolean.parseBoolean(ConfigUtil.get("SaveCommand"));
-                HttpAPI = Boolean.parseBoolean(ConfigUtil.get("Http-Api"));
-                LanOutput = Boolean.parseBoolean(ConfigUtil.get("Lan-output"));
-                CustomPort = ConfigUtil.get("Port");
-                CustomMaxPlayer = ConfigUtil.get("MaxPlayer");
-                motd = ConfigUtil.get("Motd");
+
+                // 从配置文件加载值到全局变量
+                loadPropertiesToGlobals();
+
+                System.out.println("[EasyLAN] Configuration loaded successfully from: " + file.getAbsolutePath());
+
             } catch (IOException ex) {
-                System.out.println("[EasyLAN] Error loading config file: " + ex.getMessage());
+                System.err.println("[EasyLAN] Error loading config file: " + ex.getMessage());
+                ex.printStackTrace();
+                setDefaultValues();
             }
         } else {
-            setDefaultProperties();
+            System.out.println("[EasyLAN] Config file not found, creating default configuration");
+            setDefaultValues();
             save();
         }
     }
 
     public static void save() {
-        try (OutputStream output = Files.newOutputStream(Paths.get(CONFIG_FILE))) {
-            properties.store(output, "EasyLAN configuration");
+        try {
+            // 确保目录存在
+            File configFile = new File(CONFIG_FILE);
+            configFile.getParentFile().mkdirs();
+
+            // 将全局变量的当前值保存到配置文件
+            saveGlobalsToProperties();
+
+            try (OutputStream output = Files.newOutputStream(Paths.get(CONFIG_FILE))) {
+                properties.store(output, "EasyLAN configuration");
+                System.out.println("[EasyLAN] Configuration saved successfully to: " + configFile.getAbsolutePath());
+            }
+
         } catch (IOException ex) {
-            System.out.println("[EasyLAN] Error saving config file: " + ex.getMessage());
+            System.err.println("[EasyLAN] Error saving config file: " + ex.getMessage());
+            ex.printStackTrace();
         }
     }
 
@@ -53,22 +60,80 @@ public class ConfigUtil {
 
     public static void set(String key, String value) {
         properties.setProperty(key, value);
+        System.out.println("[EasyLAN] Config set: " + key + " = " + value);
     }
 
-    private static void setDefaultProperties() {
-        properties.setProperty("Http-Api", "true");
-        properties.setProperty("Lan-output", "true");
-        properties.setProperty("pvp", "true");
-        properties.setProperty("online-mode", "true");
-        properties.setProperty("spawn-Animals", "true");
-        properties.setProperty("spawn-NPCs", "true");
-        properties.setProperty("allow-Flight", "true");
-        properties.setProperty("whiteList", "false");
-        properties.setProperty("BanCommand", "false");
-        properties.setProperty("OpCommand", "false");
-        properties.setProperty("SaveCommand", "false");
-        properties.setProperty("Motd", "This is a Default EasyLAN Motd!");
-        properties.setProperty("Port", "25565");
-        properties.setProperty("MaxPlayer", "20");
+    // 从全局变量保存到配置文件
+    private static void saveGlobalsToProperties() {
+        set("Http-Api", String.valueOf(HttpAPI));
+        set("Lan-output", String.valueOf(LanOutput));
+        set("pvp", String.valueOf(allowPVP));
+        set("online-mode", String.valueOf(onlineMode));
+        set("spawn-Animals", String.valueOf(spawnAnimals));
+        set("spawn-NPCs", String.valueOf(spawnNPCs));
+        set("allow-Flight", String.valueOf(allowFlight));
+        set("whiteList", String.valueOf(whiteList));
+        set("BanCommand", String.valueOf(BanCommand));
+        set("OpCommand", String.valueOf(OpCommand));
+        set("SaveCommand", String.valueOf(SaveCommand));
+        set("Motd", motd);
+        set("Port", CustomPort);
+        set("MaxPlayer", CustomMaxPlayer);
+    }
+
+    // 从配置文件加载到全局变量
+    private static void loadPropertiesToGlobals() {
+        allowPVP = Boolean.parseBoolean(get("pvp"));
+        onlineMode = Boolean.parseBoolean(get("online-mode"));
+        spawnAnimals = Boolean.parseBoolean(get("spawn-Animals"));
+        spawnNPCs = Boolean.parseBoolean(get("spawn-NPCs"));
+        allowFlight = Boolean.parseBoolean(get("allow-Flight"));
+        whiteList = Boolean.parseBoolean(get("whiteList"));
+        BanCommand = Boolean.parseBoolean(get("BanCommand"));
+        OpCommand = Boolean.parseBoolean(get("OpCommand"));
+        SaveCommand = Boolean.parseBoolean(get("SaveCommand"));
+        HttpAPI = Boolean.parseBoolean(get("Http-Api"));
+        LanOutput = Boolean.parseBoolean(get("Lan-output"));
+        CustomPort = get("Port");
+        CustomMaxPlayer = get("MaxPlayer");
+        motd = get("Motd");
+
+        // 确保字符串值不为 null
+        if (CustomPort == null) CustomPort = "25565";
+        if (CustomMaxPlayer == null) CustomMaxPlayer = "20";
+        if (motd == null) motd = "This is a Default EasyLAN Motd!";
+    }
+
+    private static void setDefaultValues() {
+        HttpAPI = true;
+        LanOutput = true;
+        allowPVP = true;
+        onlineMode = true;
+        spawnAnimals = true;
+        spawnNPCs = true;
+        allowFlight = true;
+        whiteList = false;
+        BanCommand = false;
+        OpCommand = false;
+        SaveCommand = false;
+        motd = "This is a Default EasyLAN Motd!";
+        CustomPort = "25565";
+        CustomMaxPlayer = "20";
+
+        // 设置默认属性
+        set("Http-Api", "true");
+        set("Lan-output", "true");
+        set("pvp", "true");
+        set("online-mode", "true");
+        set("spawn-Animals", "true");
+        set("spawn-NPCs", "true");
+        set("allow-Flight", "true");
+        set("whiteList", "false");
+        set("BanCommand", "false");
+        set("OpCommand", "false");
+        set("SaveCommand", "false");
+        set("Motd", "This is a Default EasyLAN Motd!");
+        set("Port", "25565");
+        set("MaxPlayer", "20");
     }
 }
